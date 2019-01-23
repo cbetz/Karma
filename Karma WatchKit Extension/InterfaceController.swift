7518bf03a11e452a01e6b10e9a69e8d23bc963f3
Christopher Betz <scrapdog@gmail.com>
2019-01-23 10:39:36 -0500

    Initial Commit

---
+++ Karma WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Karma WatchKit Extension
+//
+//  Created by Christopher Betz on 1/23/19.
+//  Copyright © 2019 Betz Software LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Karma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Karma
+//
+//  Created by Christopher Betz on 1/23/19.
+//  Copyright © 2019 Betz Software LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
